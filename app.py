6829886b9b52e00802d58911998a92b9fa34b2c3
Ahmed Amshoosh <amshoosh2@gmail.com>
2026-05-05 18:39:36 +0300

    fix requirements

--- app.py
+++ app.py
@@ -1,23 +1,56 @@
+from flask import Flask, render_template_string
 import xmlrpc.client
 
+app = Flask(__name__)
+
+# Odoo connection
 url = "https://azmparts.odoo.com/odoo"
 db = "azmparts"
 username = "amshoosh2@gmail.com"
 password = "772913602"
 
-# login
 common = xmlrpc.client.ServerProxy(f"{url}/xmlrpc/2/common")
 uid = common.authenticate(db, username, password, {})
 
 models = xmlrpc.client.ServerProxy(f"{url}/xmlrpc/2/object")
 
-# جلب المنتجات
-products = models.execute_kw(
-    db, uid, password,
-    'product.template',
-    'search_read',
-    [[['sale_ok', '=', True]]],
-    {'fields': ['id', 'name', 'list_price'], 'limit': 10}
-)
 
-print(products)
+@app.route('/')
+def home():
+
+    # جلب المنتجات
+    products = models.execute_kw(
+        db, uid, password,
+        'product.template',
+        'search_read',
+        [[['sale_ok', '=', True]]],
+        {'fields': ['id', 'name', 'list_price'], 'limit': 20}
+    )
+
+    # HTML بسيط لعرض البيانات
+    html = """
+    <h1>📦 Odoo Products</h1>
+    <table border="1" cellpadding="10">
+        <tr>
+            <th>ID</th>
+            <th>Name</th>
+            <th>Price</th>
+        </tr>
+    """
+
+    for p in products:
+        html += f"""
+        <tr>
+            <td>{p['id']}</td>
+            <td>{p['name']}</td>
+            <td>{p['list_price']}</td>
+        </tr>
+        """
+
+    html += "</table>"
+
+    return render_template_string(html)
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=10000)
