57364e20296eee3bfeade9b360710d0992e43b78
Ahmed Amshoosh <amshoosh2@gmail.com>
2026-05-05 19:24:10 +0300

    fix requirements

--- app.py
+++ app.py
@@ -8,27 +8,46 @@ db = "azmparts"
 username = "amshoosh2@gmail.com"
 password = "772913602"
 
+# Odoo connection
 common = xmlrpc.client.ServerProxy(f"{url}/xmlrpc/2/common", allow_none=True)
-models = xmlrpc.client.ServerProxy(f"{url}/xmlrpc/2/object")
+models = xmlrpc.client.ServerProxy(f"{url}/xmlrpc/2/object", allow_none=True)
 
 uid = common.authenticate(db, username, password, {})
 
 @app.route("/")
 def home():
 
+    if not uid:
+        return "❌ Odoo Login Failed"
+
+    fields = [
+        'id',
+        'name',
+        'list_price',
+        'default_code',
+        'barcode',
+        'description_sale',
+        'image_1920',
+        'qty_available'
+    ]
+
     products = models.execute_kw(
         db, uid, password,
-        "product.template",
-        "search_read",
-        [[["sale_ok", "=", True]]],
-        {"fields": ["id", "name", "list_price"], "limit": 40}
+        'product.template',
+        'search_read',
+        [[]],
+        {'fields': fields, 'limit': 50}
     )
 
     html = """
     <h1>Odoo Products</h1>
     <ul>
     {% for p in products %}
-        <li>{{p.name}} - {{p.list_price}}</li>
+        <li>
+            <b>{{ p.name }}</b> - {{ p.list_price }} SAR
+            <br>
+            SKU: {{ p.default_code }}
+        </li>
     {% endfor %}
     </ul>
     """
@@ -36,7 +55,7 @@ def home():
     return render_template_string(html, products=products)
 
 
-@app.route("/webhook", methods=["POST"])
+@app.route("/webhook", methods=["POST", "GET"])
 def webhook():
     return {"status": "ok"}
 
