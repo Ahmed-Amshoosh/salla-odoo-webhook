c5299abf6fbb87e19a49fd5a97f3aca6b090adf2
Ahmed Amshoosh <amshoosh2@gmail.com>
2026-05-05 19:33:17 +0300

    fix requirements

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template_string
 import xmlrpc.client
+import base64
 
 app = Flask(__name__)
 
@@ -8,7 +9,6 @@ db = "azmparts"
 username = "amshoosh2@gmail.com"
 password = "772913602"
 
-# Odoo connection
 common = xmlrpc.client.ServerProxy(f"{url}/xmlrpc/2/common", allow_none=True)
 models = xmlrpc.client.ServerProxy(f"{url}/xmlrpc/2/object", allow_none=True)
 
@@ -17,36 +17,33 @@ uid = common.authenticate(db, username, password, {})
 @app.route("/")
 def home():
 
-    if not uid:
-        return "❌ Odoo Login Failed"
-
     fields = [
-        'id',
-        'name',
-        'list_price',
-        'default_code',
-        'barcode',
-        'description_sale',
-        'image_1920',
-        'qty_available'
+        "id",
+        "name",
+        "default_code",
+        "barcode",
+        "list_price",
+        "description_sale",
+        "image_1920",
+        "categ_id"
     ]
 
     products = models.execute_kw(
         db, uid, password,
-        'product.template',
-        'search_read',
+        "product.template",
+        "search_read",
         [[]],
-        {'fields': fields, 'limit': 50}
+        {"fields": fields, "limit": 20}
     )
 
     html = """
-    <h1>Odoo Products</h1>
+    <h1>Products from Odoo</h1>
     <ul>
     {% for p in products %}
         <li>
-            <b>{{ p.name }}</b> - {{ p.list_price }} SAR
-            <br>
-            SKU: {{ p.default_code }}
+            <b>{{p.name}}</b><br>
+            SKU: {{p.default_code}}<br>
+            Price: {{p.list_price}}
         </li>
     {% endfor %}
     </ul>
